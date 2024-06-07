fefe33c212daaf633192d16d41d262485423cf17
Chenyuan Gu <robingu0519@gmail.com>
2024-06-07 20:57:43 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Display from './components/Display'
+import Player from './components/Player'
+import SideBar from './components/SideBar'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <SideBar />
+        <Display />
+      </div>
+      <Player />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {assets} from "../assets/assets"
+
+const SideBar = () => {
+  return (
+    <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
+        <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
+            <div className='flex item-center gap-3 pl-8 cursor-pointer'>
+                <img className='w-6' src={assets.home_icon} alt="home icon" />
+                <p className='font-bold'>Home</p>
+            </div>
+            <div className='flex item-center gap-3 pl-8 cursor-pointer'>
+                <img className='w-6' src={assets.search_icon} alt="search" />
+                <p className='font-bold'>Search</p>
+            </div>
+        </div>
+        <div className='bg-[#121212] h-[85%] rounded'>
+            <div className='p-4 flex item-center justify-between'>
+                <div className='flex item-center gap-3'>
+                    <img className='w-8 h-8' src={assets.stack_icon} alt="" />
+                    <p className='font-semibond'>Your Library</p>
+                </div>
+                <div className='flex item-center gap-3'>
+                    <img className='w-5 h-5' src={assets.arrow_icon} alt="" />
+                    <img className='w-5 h-5' src={assets.plus_icon} alt="" />
+                </div>
+            </div>
+            <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+                <h1>Create your first playlist</h1>
+                <p className='font-light'>it's easy we will help you</p>
+                <button className='px-4 py-1.5 bg-white text-[15p] text-black rounded-full mt-4'>Create Playlist</button>
+            </div>
+            <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+                <h1>Let's find some podcasts to follow</h1>
+                <p className='font-light'>we'll keep you update on new episodes</p>
+                <button className='px-4 py-1.5 bg-white text-[15p] text-black rounded-full mt-4'>Browse podcasts</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SideBar
